b913b767da306f3455074ca0b69a591e023437f7
Rəsul Məmmədzadə <rasul.mammadzada.std@bhos.edu.az>
2026-08-17 16:45:52 +0400

    asgi

--- core/asgi.py
+++ core/asgi.py
@@ -1,33 +1,24 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
-"""
-
-
 import os
+import sys
+import asyncio
+
+# 1. İlk növbədə settings modulunu təyin edirik
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# 2. Django-nu digər bütün Django komponentlərini və routing-i import etməzdən ƏVVƏL işə salırıq
+import django
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import messaging.routing 
-import sys
-import asyncio
-
-
 
 if sys.platform == 'win32':
     try:
-        # Python 3.15 və aşağısı üçün işləyəcək
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     except AttributeError:
-        # Python 3.16 və yuxarısında bu funksiya silinsə, heç bir xəta vermədən davam edəcək
         pass
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
